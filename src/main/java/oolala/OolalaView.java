ed48d248f2d29fe6cb1498408da01ca102e1c318
Aditya Paul <ap516@duke.edu>
2022-09-25 01:55:30 -0400

    Updated Aditya to master

--- src/main/java/oolala/OolalaView.java
+++ src/main/java/oolala/OolalaView.java
@@ -1,21 +1,24 @@
 package oolala;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+import javafx.scene.shape.Box;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.scene.layout.HBox;
 
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -32,39 +35,84 @@ public class OolalaView {
     private Paint BACKGROUND;
     private Paint BRUSH_COLOR;
     private BorderPane root;
-    private Group canvas;
     private TextBox textBox;
     private ArrayList<Command> commands;
     private Turtle turtle;
     public static ResourceBundle myResources;
     private static final String DEFAULT_RESOURCE_PACKAGE = "Properties.";
+    private Parser parser = new Parser();
+    private CanvasScreen canvasScreen;
+    private VBox canvasVBox;
+    private Group canvasShapes;
 
 
     public Scene setUpScene(int SIZE_WIDTH, int SIZE_HEIGHT, String language) {
         myResources = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + language);
         this.SIZE_WIDTH = SIZE_WIDTH;
         this.SIZE_HEIGHT = SIZE_HEIGHT;
+
         root = new BorderPane();
-        canvas = new Group();
-        root.getChildren().add(canvas);
+
         makeTextBox();
-        BorderPane.setAlignment(textBox.get(), Pos.CENTER);
-        root.setPadding(new Insets(10, 10, 10, 10));
+        //BorderPane.setAlignment(textBox.get(), Pos.CENTER);
+        // TODO: can the above commented line be deleted?
         root.setLeft(textBox.get());
-        turtle = new Turtle();
-        root.getChildren().add(turtle.getIcon());
+
+        makeCanvasScene();
+        root.setCenter(canvasVBox);
+        root.getChildren().add(canvasShapes);
+
+        root.setPadding(new Insets(10, 30, 10, 10));
+
+
+//        VBox box = new VBox();
+//        root.setRight(box);
+//        box.setPrefWidth(400);
+//        System.out.println(box.getLayoutX());
+//        System.out.println(box.getLayoutY());
+//        BorderPane.setAlignment(box, Pos.CENTER);
+//        String cssLayout = "-fx-border-color: red;\n" +
+//                "-fx-border-insets: 5;\n" +
+//                "-fx-border-width: 3;\n" +
+//                "-fx-border-style: solid;\n";
+//        box.setStyle(cssLayout);
+        //        root.setRight(box);
+
+
+//        turtle = new Turtle();
+//        root.getChildren().add(turtle.getIcon());
         Scene scene = new Scene(root, SIZE_WIDTH, SIZE_HEIGHT);
         return scene;
     }
+
+    public CanvasScreen getCanvasScreen() {
+        return canvasScreen;
+    }
+
 
     /**
      * A method to remove everything on the canvas.
      *
      * @author Luyao Wang
      */
-    private void makeTextBox(){
-        textBox = new TextBox();
+
+
+    private void makeCanvasScene(){
+        canvasScreen = new CanvasScreen();
+        canvasVBox = canvasScreen.getVBox();
+        canvasShapes = canvasScreen.getShapes();
     }
+
+    private void makeTextBox() {
+        textBox = new TextBox();
+        EventHandler<ActionEvent> passCommands = event -> {
+            ArrayList<Command> commands = parser.parse(textBox.getTextArea().getText());
+            canvasScreen.setCommands(commands, this);
+            //canvasScreen.getTurtle().readInstruction(commands.get(0), this);
+        };
+        textBox.getRunButton().setOnAction(passCommands);
+    }
+}
 //    private Node makeInputPanel() {
 //        HBox result = new HBox();
 //        EventHandler<ActionEvent> textHandler = event -> {
@@ -93,43 +141,3 @@ public class OolalaView {
 //
 //        return result;
 //    }
-
-    private void clearPaint() {
-        canvas.getChildren().clear();
-    }
-
-    private void showPaint(String text) {
-        Text showText = new Text(30, 60, text);
-        showText.setFont(new Font(15));
-        showText.setFill(Color.RED);
-        canvas.getChildren().add(showText);
-    }
-
-    /**
-     * A method to draw a new line on the canvas.
-     *
-     * @param x         x coordinate of the start point
-     * @param y         y coordinate of the end point
-     * @param length    length of the line
-     * @param direction angle between the line and the positive x-axis
-     * @author Luyao Wang
-     */
-
-    public void drawLine(double x, double y, int length, int direction, double thickness, Color color) {
-        Line line = new Line();
-        line.setStartX(SIZE_WIDTH / 2.0 + x);
-        line.setStartY(SIZE_HEIGHT / 2.0 - y);
-//        line.setStartX(x);
-//        line.setStartY(y);
-        line.setEndX(SIZE_WIDTH / 2.0 + x + length * Math.cos(Math.toRadians(direction)));
-        line.setEndY(SIZE_HEIGHT / 2.0 - y - length * Math.sin(Math.toRadians(direction)));
-//        line.setEndX(x + length * Math.cos(Math.toRadians(direction)));
-//        line.setEndY(y - length * Math.sin(Math.toRadians(direction)));
-        line.setStrokeWidth(thickness);
-        line.setFill(color);
-        canvas.getChildren().add(0, line);
-    }
-    public Group getCanvas() {
-        return canvas;
-    }
-}
